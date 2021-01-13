6f466a97dcca7dd07a44b4df3c08448f86358e8b
roman dod <stalkeroma1@mail.ru>
2021-01-14 02:02:12 +0300

    Initial Commit

---
+++ 3WaysViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3WaysViewController
+//
+//  Created by Roman Dod on 1/14/21.
+//  Copyright © 2021 Roman Dod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
